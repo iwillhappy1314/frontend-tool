c41bb20d9f61eaf77bd51f362d55fcd41e1c3324
Amos Lee <iwillhappy1314@gmail.com>
2023-08-23 10:29:58 +0800

    fix bootstrap dropdown toggle conflict with menu dropdown

--- assets/scripts/components/menu-toggle.js
+++ assets/scripts/components/menu-toggle.js
@@ -1,5 +1,5 @@
 export default function menuToggle() {
-    $('.rs-dropdown-toggle').click(function() {
+    $('.rs-dropdown-nav-toggle').click(function() {
         $('body').toggleClass('f-menu-toggled');
         $('.f-body-cover').toggleClass('hidden');
         $('#site-navigation').toggleClass('is-toggled');
