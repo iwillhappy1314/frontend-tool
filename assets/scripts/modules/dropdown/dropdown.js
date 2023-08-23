83e7ef357d6ba612f533478bc72420661d317bb0
Amos Lee <iwillhappy1314@gmail.com>
2023-08-23 11:11:56 +0800

    add dropdown css

--- assets/scripts/modules/dropdown/dropdown.js
+++ assets/scripts/modules/dropdown/dropdown.js
@@ -1,15 +1,9 @@
 'use strict';
 import Dropdown from 'bootstrap/js/dist/dropdown';
-import Popover from 'bootstrap/js/dist/popover';
+import './dropdown.scss';
 
 const dropdownElementList = document.querySelectorAll('.rs-dropdown-toggle');
 
 [...dropdownElementList].map(function(dropdownToggleEl) {
     new Dropdown(dropdownToggleEl);
-});
-
-const popoverElementList = document.querySelectorAll('.rs-popover');
-
-[...popoverElementList].map(function(popoverEl) {
-    new Popover(popoverEl);
 });
