3990a8365965a9a69b3168be23b44a7d8cba1947
Amos Lee <iwillhappy1314@gmail.com>
2024-05-14 14:54:13 +0800

    更新 Tailwind 设置

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,19 +72,19 @@ module.exports = {
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
 
-      'widescreen': {'max': '1535px'},
+      '-2xl': {'max': '1535px'},
       // => @media (max-width: 1535px) { ... }
 
-      'desktop': {'max': '1279px'},
+      '-xl': {'max': '1279px'},
       // => @media (max-width: 1279px) { ... }
 
-      'laptop': {'max': '1023px'},
+      '-lg': {'max': '1023px'},
       // => @media (max-width: 1023px) { ... }
 
-      'tablet': {'max': '767px'},
+      '-md': {'max': '767px'},
       // => @media (max-width: 767px) { ... }
 
-      'phone': {'max': '639px'},
+      '-sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
     },
     container: {
